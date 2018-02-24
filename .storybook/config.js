aeb7678903dbbade5b7c8082c5cf8fc382aa6eca
Luis Fernando Díaz <ldiaz@occ.com.mx>
2018-02-23 18:14:36 -0600

    Se agrega storybook de botón, y función para agregar iconos svg (sin base64) y cambiar sus colores

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,10 @@
 import { configure } from '@storybook/react';
+import { setOptions } from '@storybook/addon-options';
+// import '@storybook/addon-console';
+
+setOptions({
+    addonPanelInRight: true
+});
 
 function loadStories() {
     require('../stories/elements/Button.js');
