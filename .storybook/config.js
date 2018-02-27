651a9dba057c3fc4731a2a1b2f6e7f3ca96ec958
Luis Fernando Díaz <bloodysummers@gmail.com>
2018-02-27 16:30:47 -0600

    Se crean componentes de Card, Title y ButtonAlign
    El componente de Card puede recibir como prop noPadding si no necesita padding arriba y abajo, y el nivel de sombra con el prop shadow.
    Title recibe como prop el nivel de heading. ButtonAlign recibe como prop el tamaño del botón, sirve para alinear el botón con otros input.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,5 @@
 import { configure } from '@storybook/react';
 import { setOptions } from '@storybook/addon-options';
-// import '@storybook/addon-console';
 
 setOptions({
     addonPanelInRight: true
