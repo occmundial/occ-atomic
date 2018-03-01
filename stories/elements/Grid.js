6380b2445de79e64d78d46f1d363cc85c96855ca
Luis Fernando Díaz <bloodysummers@gmail.com>
2018-03-01 11:00:39 -0600

    Fix en estilos y knobs del story de grid

--- stories/elements/Grid.js
+++ stories/elements/Grid.js
@@ -17,19 +17,19 @@ const sizeOptions = {
 stories.add('Container, Row & Column', () => (
         <div>
             <h1 style={{fontFamily:'Open Sans'}}>Container, Row & Column</h1>
-            <Container styles={{fontFamily:'Open Sans', background:'#f7f7f7'}}>
+            <Container style={{fontFamily:'Open Sans', background:'#f7f7f7'}}>
                 Container
-                <Row styles={{background:'#e7e7e7'}}>
+                <Row style={{background:'#e7e7e7'}}>
                     Row
                 </Row>
                 <Row>
-                    <Column xs={{col:6}} styles={{background:'#d7d7d7'}}>
+                    <Column xs={{col:6}} style={{background:'#d7d7d7'}}>
                         Column
                         <div style={{background:'#b7b7b7'}}>
                             Column content
                         </div>
                     </Column>
-                    <Column xs={{col:6}} styles={{background:'#d7d7d7'}}>
+                    <Column xs={{col:6}} style={{background:'#d7d7d7'}}>
                         Column
                         <div style={{background:'#b7b7b7'}}>
                             Column content
@@ -41,7 +41,7 @@ stories.add('Container, Row & Column', () => (
     )).add('Breakpoints', () => (
         <div>
             <h1 style={{fontFamily:'Open Sans'}}>Breakpoints</h1>
-            <Container styles={{fontFamily:'Open Sans', background:'#f7f7f7'}}>
+            <Container style={{fontFamily:'Open Sans', background:'#f7f7f7'}}>
                 <Row>
                     <Column xs={{col:12}} sm={{col:8}} md={{col:6}} lg={{col:4}}>
                         <div style={{background:'#b7b7b7'}}>
@@ -81,22 +81,22 @@ stories.add('Container, Row & Column', () => (
     )).add('Hidden & Visible', () => (
         <div>
             <h1 style={{fontFamily:'Open Sans'}}>Hidden & Visible</h1>
-            <Container styles={{fontFamily:'Open Sans', background:'#f7f7f7'}}>
+            <Container style={{fontFamily:'Open Sans', background:'#f7f7f7'}}>
                 <Row>
-                    <Column xs={{col:12}} styles={{background:'#d7d7d7'}}>
+                    <Column xs={{col:12}} style={{background:'#d7d7d7'}}>
                         <div style={{background:'#b7b7b7'}}>
                             Always visible
                         </div>
                     </Column>
                     <Hidden xs>
-                        <Column xs={{col:12}} styles={{background:'#d7d7d7'}}>
+                        <Column xs={{col:12}} style={{background:'#d7d7d7'}}>
                             <div style={{background:'#b7b7b7'}}>
                                 Hidden in XS
                             </div>
                         </Column>
                     </Hidden>
                     <Visible md>
-                        <Column xs={{col:12}} styles={{background:'#d7d7d7'}}>
+                        <Column xs={{col:12}} style={{background:'#d7d7d7'}}>
                             <div style={{background:'#b7b7b7'}}>
                                 Visible in MD
                             </div>
@@ -108,33 +108,33 @@ stories.add('Container, Row & Column', () => (
     )).add('Offset, Push & Pull', () => (
         <div>
             <h1 style={{fontFamily:'Open Sans'}}>Offset, Push & Pull</h1>
-            <Container styles={{fontFamily:'Open Sans', background:'#f7f7f7'}}>
+            <Container style={{fontFamily:'Open Sans', background:'#f7f7f7'}}>
                 <Row>
-                    <Column xs={{col:6, offset:4}} styles={{background:'#d7d7d7'}}>
+                    <Column xs={{col:6, offset:4}} style={{background:'#d7d7d7'}}>
                         <div style={{background:'#b7b7b7'}}>
                             Offset 4 columns
                         </div>
                     </Column>
                 </Row><br />
                 <Row>
-                    <Column xs={{col:3}} styles={{background:'#d7d7d7'}}>
+                    <Column xs={{col:3}} style={{background:'#d7d7d7'}}>
                         <div style={{background:'#b7b7b7'}}>
                             3 columns
                         </div>
                     </Column>
-                    <Column xs={{col:6, push:3}} styles={{background:'#d7d7d7'}}>
+                    <Column xs={{col:6, push:3}} style={{background:'#d7d7d7'}}>
                         <div style={{background:'#b7b7b7'}}>
                             Push 3 columns
                         </div>
                     </Column>
                 </Row><br />
                 <Row>
-                    <Column xs={{col:3}} styles={{background:'#d7d7d7'}}>
+                    <Column xs={{col:3}} style={{background:'#d7d7d7'}}>
                         <div style={{background:'#b7b7b7'}}>
                             3 columns
                         </div>
                     </Column>
-                    <Column xs={{col:6, pull:1}} styles={{background:'#d7d7d7'}}>
+                    <Column xs={{col:6, pull:1}} style={{background:'#d7d7d7'}}>
                         <div style={{background:'#b7b7b7'}}>
                             Pull 1 column
                         </div>
@@ -144,37 +144,34 @@ stories.add('Container, Row & Column', () => (
         </div>
     )).add('Playground', (() => {
         const range = {
-            min:1,
-            max:12
-        }
-        const range2 = {
             min:0,
             max:12
         }
+        const conFluid = boolean('Container fluid', false);
         const useVisible = boolean('Use <Visible>', false);
         const visible = select('Visible in', sizeOptions);
         const useHidden = boolean('Use <Hidden>', false);
         const hidden = select('Hidden in', sizeOptions);
         const xsCol = number('xs columns', 4, range);
-        const xsOff = number('xs offset', 0, range2);
-        const xsPush = number('xs push', 0, range2);
-        const xsPull = number('xs pull', 0, range2);
+        const xsOff = number('xs offset', 0, range);
+        const xsPush = number('xs push', 0, range);
+        const xsPull = number('xs pull', 0, range);
         const smCol = number('sm columns', 4, range);
-        const smOff = number('sm offset', 0, range2);
-        const smPush = number('sm push', 0, range2);
-        const smPull = number('sm pull', 0, range2);
+        const smOff = number('sm offset', 0, range);
+        const smPush = number('sm push', 0, range);
+        const smPull = number('sm pull', 0, range);
         const mdCol = number('md columns', 4, range);
-        const mdOff = number('md offset', 0, range2);
-        const mdPush = number('md push', 0, range2);
-        const mdPull = number('md pull', 0, range2);
+        const mdOff = number('md offset', 0, range);
+        const mdPush = number('md push', 0, range);
+        const mdPull = number('md pull', 0, range);
         const lgCol = number('lg columns', 4, range);
-        const lgOff = number('lg offset', 0, range2);
-        const lgPush = number('lg push', 0, range2);
-        const lgPull = number('lg pull', 0, range2);
+        const lgOff = number('lg offset', 0, range);
+        const lgPush = number('lg push', 0, range);
+        const lgPull = number('lg pull', 0, range);
         const Wrapper = useVisible ? Visible : useHidden ? Hidden : 'div';
         return (<div>
             <h1 style={{fontFamily:'Open Sans'}}>Playground</h1>
-                <Container styles={{fontFamily:'Open Sans', background:'#f7f7f7'}}>
+                <Container fluid={conFluid} style={{fontFamily:'Open Sans', background:'#f7f7f7'}}>
                     <Row>
                         <Wrapper
                             xs={((useVisible && visible=='xs') || (useHidden && hidden=='xs')) ? true : undefined}
@@ -187,7 +184,7 @@ stories.add('Container, Row & Column', () => (
                                 sm={{col: smCol, offset: smOff, push: smPush, pull: smPull}}
                                 md={{col: mdCol, offset: mdOff, push: mdPush, pull: mdPull}}
                                 lg={{col: lgCol, offset: lgOff, push: lgPush, pull: lgPull}}
-                                styles={{background:'#d7d7d7'}}>
+                                style={{background:'#d7d7d7'}}>
                                 Column 1
                             </Column>
                         </Wrapper>
@@ -195,11 +192,17 @@ stories.add('Container, Row & Column', () => (
                 </Container>
                 <pre style={{background:'#ddd', padding:'15px', overflowX:'auto'}}>
                     <code>
-                        {/* {`import { Card } from 'r12-common';
+                        {`import { Container, Row, Column, Hidden, Visible } from 'r12-common';
 
-<Card${shadow ? ` shadow="${shadow}"` : ''}${padding ? ' noPadding' : ''}>
-    Card content
-</card>`} */}
+<Container${conFluid ? ` fluid` : ''}>
+    <Row>
+        ${useVisible ? `<Visible${visible ? ` ${visible}` : ''}>` : useHidden ? `<Hidden${hidden ? ` ${hidden}` : ''}>` : ''}
+            <Column${(xsCol || xsOff || xsPush || xsPull) ? ` xs={{${xsCol ? `col:${xsCol},` : ''}${xsOff ? `offset:${xsOff},` : ''}${xsPush ? `push:${xsPush},` : ''}${xsPull ? `pull:${xsPull},` : ''}}}` : ''}${(smCol || smOff || smPush || smPull) ? ` sm={{${smCol ? `col:${smCol},` : ''}${smOff ? `offset:${smOff},` : ''}${smPush ? `push:${smPush},` : ''}${smPull ? `pull:${smPull},` : ''}}}` : ''}${(mdCol || mdOff || mdPush || mdPull) ? ` md={{${mdCol ? `col:${mdCol},` : ''}${mdOff ? `offset:${mdOff},` : ''}${mdPush ? `push:${mdPush},` : ''}${mdPull ? `pull:${mdPull},` : ''}}}` : ''}${(lgCol || lgOff || lgPush || lgPull) ? ` lg={{${lgCol ? `col:${lgCol},` : ''}${lgOff ? `offset:${lgOff},` : ''}${lgPush ? `push:${lgPush},` : ''}${lgPull ? `pull:${lgPull},` : ''}}}` : ''}>
+                Column 1
+            </Column>
+        ${useVisible ? `</Visible>` : useHidden ? `</Hidden>` : ''}
+    </Row>
+</Container>`}
                     </code>
                 </pre>
         </div>)
