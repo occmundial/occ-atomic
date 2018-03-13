4858d75bd96b517d780cec92ec563e747d23e623
Luis Fernando Díaz <bloodysummers@gmail.com>
2018-03-13 13:10:16 -0600

    Ajuste en Autocomplete, y se agregan stories de Autocomplete al storybook

--- stories/molecules/Autocomplete.js
+++ stories/molecules/Autocomplete.js
@@ -26,6 +26,7 @@ class AutocompleteWrapper extends React.Component {
     }
     
     render() {
+        const { type, knobs } = this.props;
         const items = [
             {
                 text: 'Administrador',
@@ -44,30 +45,150 @@ class AutocompleteWrapper extends React.Component {
                 textRight: '(6)'
             },
         ];
-        return (
-            <div>
-                <Autocomplete
-                    name="default"
-                    label="Label"
-                    placeholder="Placeholder"
-                    limitPlaceholder="Limit of 2 options"
-                    items={items}
-                    itemTextKey="text"
-                    itemTextRightKey="textRight"
-                    itemIdKey="text"
-                    onChange={this.onChange}
-                    onClick={this.onClick}
-                    stackLabels
-                    limit={2}
-                />
-            </div>
-        );
+        if (type == "default") {
+            return (
+                <div>
+                    <Autocomplete
+                        label="Label"
+                        placeholder="Placeholder"
+                        items={items}
+                        itemTextKey="text"
+                        itemTextRightKey="textRight"
+                        itemIdKey="text"
+                        onChange={this.onChange}
+                    />
+                </div>
+            );
+        } else if (type == "item") {
+            return (
+                <div>
+                    <Autocomplete
+                        label="Label"
+                        placeholder="Placeholder"
+                        items={items}
+                        itemTextKey="text"
+                        itemTextRightKey="textRight"
+                        itemIdKey="text"
+                        onClick={this.onClick}
+                    />
+                </div>
+            );
+        } else if (type == "stack") {
+            return (
+                <div>
+                    <Autocomplete
+                        label="Label"
+                        placeholder="Placeholder"
+                        limitPlaceholder="Limit of 2 options"
+                        items={items}
+                        itemTextKey="text"
+                        itemTextRightKey="textRight"
+                        itemIdKey="text"
+                        onClick={this.onClick}
+                        stackLabels
+                        limit={2}
+                    />
+                </div>
+            );
+        } else if (type == "playground") {
+            return (
+                <div>
+                    <Autocomplete
+                        label={knobs.label}
+                        placeholder={knobs.placeholder}
+                        onFocus={knobs.onFocus ? () => {console.log('onFocus');} : () => {}}
+                        onChange={knobs.onChange ? (value) => {console.log('onChange', value);} : () => {}}
+                        onBlur={knobs.onBlur ? () => {console.log('onBlur');} : () => {}}
+                        items={JSON.parse(knobs.items)}
+                        groups={knobs.groups}
+                        groupNameKey={knobs.groupNameKey}
+                        groupIdKey={knobs.groupIdKey}
+                        groupItemsKey={knobs.groupItemsKey}
+                        itemTextKey={knobs.itemTextKey}
+                        itemTextRightKey={knobs.itemTextRightKey}
+                        itemIdKey={knobs.itemIdKey}
+                        onClick={knobs.onClick ? (item) => {console.log('onClick', item);} : () => {}}
+                        stackLabels={knobs.stackLabels}
+                        limit={knobs.limit}
+                        limitPlaceholder={knobs.limitPlaceholder}
+                    />
+                </div>
+            );
+        }
     }
 }
 
-stories.add('Autocomplete', () => (
+stories.add('Default', () => (
         <div>
-            <h1 style={{fontFamily:'Open Sans'}}>Autocomplete</h1>
-            <AutocompleteWrapper />
+            <h1 style={{fontFamily:'Open Sans'}}>Default</h1>
+            <AutocompleteWrapper type="default" />
         </div>
-    ))
+    )).add('Return item', () => (
+        <div>
+            <h1 style={{fontFamily:'Open Sans'}}>Return item</h1>
+            <AutocompleteWrapper type="item" />
+        </div>
+    )).add('Stack labels', () => (
+        <div>
+            <h1 style={{fontFamily:'Open Sans'}}>Stack labels</h1>
+            <AutocompleteWrapper type="stack" />
+        </div>
+    )).add('Playground', () => {
+        const label = text('label', 'Label');
+        const placeholder = text('placeholder', 'Placeholder');
+        const onFocus = boolean('onFocus', false);
+        const onChange = boolean('onChange', false);
+        const onBlur = boolean('onBlur', false);
+        const items = text('items', JSON.stringify(
+            [
+                {
+                    item: 'text1',
+                    right: '(201)'
+                },
+                {
+                    item: 'text2',
+                    right: '(305)'
+                },
+                {
+                    item: 'text3',
+                    right: '(2)'
+                }
+            ]
+        ));
+        const groups = boolean('groups', false);
+        const groupNameKey = text('groupNameKey', '');
+        const groupIdKey = text('groupIdKey', '');
+        const groupItemsKey = text('groupItemsKey', '');
+        const itemTextKey = text('itemTextKey', 'item');
+        const itemTextRightKey = text('itemTextRightKey', '');
+        const itemIdKey = text('itemIdKey', 'item');
+        const onClick = boolean('onClick', false);
+        const stackLabels = boolean('stackLabels', false);
+        const limit = number('limit', 2);
+        const limitPlaceholder = text('limitPlaceholder', '');
+        const knobsObj = {
+            label,
+            placeholder,
+            onFocus,
+            onChange,
+            onBlur,
+            items,
+            groups,
+            groupNameKey,
+            groupIdKey,
+            groupItemsKey,
+            itemTextKey,
+            itemTextRightKey,
+            itemIdKey,
+            onClick,
+            stackLabels,
+            limit,
+            limitPlaceholder
+        };
+        return (
+            <div>
+                <h1 style={{fontFamily:'Open Sans'}}>Playground</h1>
+                <AutocompleteWrapper type="playground" knobs={knobsObj} />
+            </div>
+        )
+    })
