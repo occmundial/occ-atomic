763b2f2915f92241fc1cadad8d86600c29a0f22d
Luis Fernando Díaz <bloodysummers@gmail.com>
2018-06-04 10:51:38 -0500

    Se agregan ejemplos a SightLogo y a Title

---
+++ src/docs/examples/SightLogo/Example1.js
@@ -0,0 +1,11 @@
+/* SightLogo */
+import React from 'react';
+import { SightLogo, colors } from 'r12-common';
+
+export default function Example1() {
+    return (
+        <div style={{background:colors.blue, padding:'15px 30px 70px 30px'}}>
+            <SightLogo />
+        </div>
+    )
+}
---
+++ src/docs/examples/Title/Example1.js
@@ -0,0 +1,9 @@
+/* Title with h1 */
+import React from 'react';
+import { Title } from 'r12-common';
+
+export default function Example1() {
+    return (
+        <Title h={1}>Title</Title>
+    )
+}
