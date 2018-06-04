206dab867e338920688238019f79091eec219ae2
Luis Fernando Díaz <bloodysummers@gmail.com>
2018-06-04 17:43:03 -0500

    Se agregan ejemplos para Hidden y Visible

---
+++ src/docs/examples/Hidden/Example1.js
@@ -0,0 +1,9 @@
+/* Hide content in small window size */
+import React from 'react';
+import { Hidden } from 'r12-common';
+
+export default function Example1() {
+    return (
+        <Hidden sm>Content</Hidden>
+    )
+}
---
+++ src/docs/examples/Visible/Example1.js
@@ -0,0 +1,9 @@
+/* Show content in medium window size */
+import React from 'react';
+import { Visible } from 'r12-common';
+
+export default function Example1() {
+    return (
+        <Visible md>Content</Visible>
+    )
+}
