756278938bb3ec5bc523a94edc0334bcc97b8d18
Luis Fernando Díaz <bloodysummers@gmail.com>
2018-06-13 18:03:09 -0500

    Updates

--- subatomic/fonts.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-exports.default = {
-    body: "'Open Sans', sans-serif",
-    title: "'Nunito Sans', sans-serif"
-};
