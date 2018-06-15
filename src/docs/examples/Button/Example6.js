93c68551a8aa13a70c3feea97059d169c15279d0
Luis Fernando Díaz <ldiaz@occ.com.mx>
2018-06-15 12:15:46 -0500

    Se agregan ejemplos

---
+++ src/docs/examples/Button/Example6.js
@@ -0,0 +1,9 @@
+/* Button disabled */
+import React from 'react';
+import { Button } from '@occmundial/occ-atomic';
+
+export default function Example6() {
+    return (
+        <Button disabled>Disabled</Button>
+    ) 
+}
---
+++ src/docs/examples/SlideDown/Example1.js
@@ -0,0 +1,11 @@
+/* Simple SlideDown */
+import React from 'react';
+import { SlideDown } from '@occmundial/occ-atomic';
+
+export default function Example1() {
+    return (
+        <SlideDown title="SlideDown">
+            Content of the SlideDown component
+        </SlideDown>
+    )
+}
