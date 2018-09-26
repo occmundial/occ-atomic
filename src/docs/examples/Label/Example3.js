e3acd19a4a019110f982baa1aeeb99113207c561
Luis Fernando Díaz <ldiaz@occ.com.mx>
2018-09-26 13:22:28 -0500

    New blue theme for the Label component

---
+++ src/docs/examples/Label/Example3.js
@@ -0,0 +1,9 @@
+/* Label with theme */
+import React from 'react';
+import { Label } from '@occmundial/occ-atomic';
+
+export default function Example3() {
+    return (
+        <Label theme="blue">Label with theme</Label>
+    );
+}
