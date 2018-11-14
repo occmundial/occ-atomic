d57d3c8956264946cc83af890f9bd13470237632
ldiaz-occ <ldiaz@occ.com.mx>
2018-11-14 10:11:00 -0600

    New Button themes. Fixes in TextField disabled status. New spacing value.

--- src/docs/examples/Button/Example1.js
+++ src/docs/examples/Button/Example1.js
@@ -5,12 +5,13 @@ import { Button, colors } from '@occmundial/occ-atomic';
 export default function Example1() {
     return (
         <div>
-            <Button theme="main">Main</Button>
+            <Button theme="primary">Primary</Button>
             <Button theme="secondary">Secondary</Button>
-            <Button theme="blue">Blue</Button>
-            <Button theme="main">Flat</Button>
-            <div style={{background:colors.grey7, marginTop:'15px', padding:'10px', display:'inline-block'}}>
-                <Button theme="white">White</Button>
+            <Button theme="tertiary">Tertiary</Button>
+            <Button theme="ghostPink">Ghost Pink</Button>
+            <Button theme="ghostGrey">Ghost Grey</Button>
+            <div style={{background:colors.sec, marginTop:'15px', padding:'10px', display:'inline-block'}}>
+                <Button theme="ghostWhite">Ghost White</Button>
             </div>
         </div>
     );
