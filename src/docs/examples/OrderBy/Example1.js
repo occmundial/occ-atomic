562df8d6545f64e8f841d45e310868a5b9cbeeb0
ldiaz-occ <ldiaz@occ.com.mx>
2019-04-12 16:22:46 -0500

    Changes in OrderBy component

--- src/docs/examples/OrderBy/Example1.js
+++ src/docs/examples/OrderBy/Example1.js
@@ -2,13 +2,35 @@
 import React from 'react';
 import { OrderBy } from '@occmundial/occ-atomic';
 
-export default function Example1() {
-    const options = [
-        {value:1, text:'Price'},
-        {value:2, text:'Date'},
-        {value:3, text:'Name'}
-    ];
-    return (
-        <OrderBy text="Order by:" options={options} />
-    );
+export default class Example1 extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            selected: 1
+        };
+        this.changeSelected = this.changeSelected.bind(this);
+    }
+
+    changeSelected(selected) {
+        this.setState({
+            selected
+        });
+    }
+
+    render() {
+        const { selected } = this.state;
+
+        const options = [
+            {value:1, text:'Price'},
+            {value:2, text:'Date'},
+            {value:3, text:'Name'}
+        ];
+
+        return (
+            <div style={{ float:'right' }}>
+                <OrderBy text="Order by" options={options} selected={selected} onChange={this.changeSelected} />
+            </div>
+        );
+    }
 }
