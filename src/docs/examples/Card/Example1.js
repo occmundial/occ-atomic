7bbf5de1803f9ef698269116c7e6614e3f707dec
ldiaz-occ <ldiaz@occ.com.mx>
2019-05-03 11:09:47 -0500

    Changes in styles of Card

--- src/docs/examples/Card/Example1.js
+++ src/docs/examples/Card/Example1.js
@@ -1,11 +1,26 @@
-/* Card with a shadow of level 5 */
+/* Elevations of card */
 import React from 'react';
-import { Card, Title } from '@occmundial/occ-atomic';
+import { Card, Text, spacing } from '@occmundial/occ-atomic';
 
 export default function Example1() {
     return (
-        <Card shadow={5}>
-            <Title h={3}>Card content</Title>
-        </Card>
+        <div>
+            <Card shadow={1} style={{ marginBottom: spacing.base }}>
+                <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium pretium tempor.</Text>
+            </Card>
+            <Card shadow={2} style={{ marginBottom: spacing.base }}>
+                <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium pretium tempor.</Text>
+            </Card>
+            <Card shadow={3} style={{ marginBottom: spacing.base }}>
+                <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium pretium tempor.</Text>
+            </Card>
+            <Card shadow={4} style={{ marginBottom: spacing.base }}>
+                <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium pretium tempor.</Text>
+            </Card>
+            <Card shadow={5} style={{ marginBottom: spacing.base }}>
+                <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium pretium tempor.</Text>
+            </Card>
+        </div>
+
     );
 }
