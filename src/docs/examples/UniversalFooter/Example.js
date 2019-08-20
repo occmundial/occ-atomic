016bc68540b0230f44745d8b863079ed1a105f52
Fernando Romero Tirado <fernando.romero@occ.com.mx>
2019-08-20 17:04:00 -0500

    WIP footer:  Se agrega seccion Presencia y se hacen correcciones de estilo en sección inferior.

--- src/docs/examples/UniversalFooter/Example.js
+++ src/docs/examples/UniversalFooter/Example.js
@@ -1,12 +1,7 @@
+/*Universal Footer*/
 import React from "react";
 import { UniversalFooter } from "@occmundial/occ-atomic";
 
-
 export default function Example() {
-    return (
-        <div style={{position:'relative'}}>
-            <div id="target" style={{width:100, height:100, background:'#f7f7f7', display:'flex', textAlign:'center', alignItems:'center'}}>Some content</div>
-            <UniversalFooter/>
-        </div>
-    );
+  return <UniversalFooter />;
 }
