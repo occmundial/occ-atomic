5838d4cfe3dd214fcd17dda29326475a2a63ad7a
Luis Fernando Díaz <ldiaz@occ.com.mx>
2019-08-23 11:52:57 -0500

    Changes in the Card api about elevations

--- src/docs/examples/Card/Example1.js
+++ src/docs/examples/Card/Example1.js
@@ -5,19 +5,13 @@ import { Card, Text, spacing } from '@occmundial/occ-atomic';
 export default function Example1() {
     return (
         <div>
-            <Card shadow={1} style={{ marginBottom: spacing.base }}>
+            <Card rest style={{ marginBottom: spacing.base }}>
                 <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium pretium tempor.</Text>
             </Card>
-            <Card shadow={2} style={{ marginBottom: spacing.base }}>
+            <Card style={{ marginBottom: spacing.base }}>
                 <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium pretium tempor.</Text>
             </Card>
-            <Card shadow={3} style={{ marginBottom: spacing.base }}>
-                <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium pretium tempor.</Text>
-            </Card>
-            <Card shadow={4} style={{ marginBottom: spacing.base }}>
-                <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium pretium tempor.</Text>
-            </Card>
-            <Card shadow={5} style={{ marginBottom: spacing.base }}>
+            <Card raised style={{ marginBottom: spacing.base }}>
                 <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium pretium tempor.</Text>
             </Card>
         </div>
