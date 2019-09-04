8f30acb551c912256a82eb94ed51e0e4d985b919
Fernando Romero Tirado <fernando.romero@occ.com.mx>
2019-09-04 12:52:05 -0500

    WIP: Footer parametrizable.

--- src/config/componentOrdering.js
+++ src/config/componentOrdering.js
@@ -43,7 +43,8 @@ module.exports = [
         name: 'Organisms',
         components: [
             'Footer',
-            'Header'
+            'Header',
+            'UniversalFooter'
         ]
     },
     {
--- src/docs/examples/UniversalFooter/Example.js
+++ src/docs/examples/UniversalFooter/Example.js
@@ -2,6 +2,111 @@
 import React from "react";
 import { UniversalFooter } from "@occmundial/occ-atomic";
 
+const columns = [
+  [{
+    key: 0,
+    title: "Empresas",
+    items: [
+      {
+        type: "link",
+        text: "Busco talento / Publicar Vacante",
+        href: 'http://occ.com.mx',
+        key: "1",
+        target: "_self"
+      },
+      {
+        type: "link",
+        text: "Busco talento 2 / Publicar Vacante",
+        href: 'http://occ.com.mx',
+        key: "2",
+        target: "_blank"
+      },
+      {
+        type: "link",
+        text: "Busco talento 3/ Publicar Vacante",
+        href: 'http://occ.com.mx',
+        key: "3"
+      }
+    ]
+  }],
+  [{
+    key: 1,
+    title: "Empresas",
+    items: [
+      {
+        type: "link",
+        text: "Busco talento / Publicar Vacante",
+        href: `holi/`,
+        key: "1",
+        target: "_self"
+      },
+      {
+        type: "link",
+        text: "Busco talento 2 / Publicar Vacante",
+        href: `holi2/`,
+        key: "2",
+        target: "_blank"
+      },
+      {
+        type: "link",
+        text: "Busco talento 3/ Publicar Vacante",
+        href: `holi23/`,
+        key: "3"
+      },
+      {
+        key: "4",
+        type: "icon",
+        iconName: "skypeSolid",
+        onClick: () => {}
+      },
+      {
+        key: "5",
+        type: "icon",
+        iconName: "twitterSolid",
+        onClick: () => {}
+      },
+      {
+        key: "6",
+        type: "icon",
+        iconName: "facebookSolid",
+        href: "fb.com",
+        target: "_blank",
+        onClick: () => (window.location = "holi.com")
+      },
+      {
+        key: "7",
+        type: "custom",
+        custom: (
+          <a href="#">
+            <img src="http://placehold.it/70x30" />
+          </a>
+        )
+      }
+    ]
+  },
+  {
+    key: 2,
+    title: "No Empresas",
+    collapse: true,
+    items: [
+      {
+        type: "link",
+        text: "Busco talento / Publicar Vacante",
+        href: `holi/`,
+        key: "1",
+        target: "_self"
+      },
+      {
+        type: "link",
+        text: "Busco talento 2 / Publicar Vacante",
+        href: `holi2/`,
+        key: "2",
+        target: "_blank"
+      }
+    ]
+  }]
+];
+
 export default function Example() {
-  return <UniversalFooter />;
+  return <UniversalFooter columns={columns} />;
 }
