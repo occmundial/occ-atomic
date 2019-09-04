98b93b7969c8f80371962b0a4c302b42a77bf542
Miguel Malagon <miguelmalagon@outlook.com>
2019-09-04 17:53:58 -0500

    update text on example

--- src/docs/examples/UniversalFooter/Example.js
+++ src/docs/examples/UniversalFooter/Example.js
@@ -6,27 +6,33 @@ const columns = [
 	[
 		{
 			key: 0,
-			title: 'Empresas',
+			title: 'Candidatos',
 			items: [
 				{
 					type: 'link',
-					text: 'Busco talento / Publicar Vacante',
+					text: 'Crear cuenta grátis',
 					href: 'http://occ.com.mx',
 					key: '1',
 					target: '_self'
 				},
 				{
 					type: 'link',
-					text: 'Busco talento 2 / Publicar Vacante',
+					text: 'Iniciar Sesión',
 					href: 'http://occ.com.mx',
 					key: '2',
 					target: '_blank'
 				},
 				{
 					type: 'link',
-					text: 'Busco talento 3/ Publicar Vacante',
+					text: 'Perfiles laborales',
 					href: 'http://occ.com.mx',
 					key: '3'
+				},
+				{
+					type: 'link',
+					text: 'Preguntas frecuentes',
+					href: 'http://occ.com.mx',
+					key: '4'
 				}
 			]
 		}
@@ -34,26 +40,26 @@ const columns = [
 	[
 		{
 			key: 1,
-			title: 'Empresas',
+			title: 'Empleos por clasificación',
 			items: [
 				{
 					type: 'link',
-					text: 'Busco talento / Publicar Vacante',
-					href: `holi/`,
+					text: 'Por estado',
+					href: 'http://occ.com.mx',
 					key: '1',
 					target: '_self'
 				},
 				{
 					type: 'link',
-					text: 'Busco talento 2 / Publicar Vacante',
-					href: `holi2/`,
+					text: 'Por ciudad',
+					href: 'http://occ.com.mx',
 					key: '2',
 					target: '_blank'
 				},
 				{
 					type: 'link',
-					text: 'Busco talento 3/ Publicar Vacante',
-					href: `holi23/`,
+					text: 'Por categoría',
+					href: 'http://occ.com.mx',
 					key: '3'
 				},
 				{
@@ -89,20 +95,20 @@ const columns = [
 		},
 		{
 			key: 2,
-			title: 'No Empresas',
+			title: 'Presencia mundial',
 			collapse: true,
 			items: [
 				{
 					type: 'link',
-					text: 'Busco talento / Publicar Vacante',
-					href: `holi/`,
+					text: 'OCCListing',
+					href: 'http://occ.com.mx',
 					key: '1',
 					target: '_self'
 				},
 				{
 					type: 'link',
-					text: 'Busco talento 2 / Publicar Vacante',
-					href: `holi2/`,
+					text: 'OCCEducación',
+					href: 'http://occ.com.mx',
 					key: '2',
 					target: '_blank'
 				}
@@ -112,9 +118,9 @@ const columns = [
 ];
 
 const bottomLinks = [
-	{ href: 'localhost', text: 'item 1', key: 1 },
-	{ href: 'localhost', text: 'item 2', key: 2 },
-	{ href: 'localhost', text: 'item 2', key: 3 }
+	{ href: 'http://occ.com.mx', text: 'Términos y condiciones', key: 1 },
+	{ href: 'http://occ.com.mx', text: 'Aviso de privacidad', key: 2 },
+	{ href: 'http://occ.com.mx', text: 'Mapa del sitio', key: 3 }
 ];
 
 const copy = 'OCCMundial D.R. © 1996-2018 Derechos reservados.';
