27da89c93d842dc70dac29416932340aa6240963
Moises Rojas <irojas@occ.com.mx>
2019-11-26 15:35:19 -0600

    Se actualizan examples y el bullet default

--- src/docs/examples/ListItem/Example2.js
+++ src/docs/examples/ListItem/Example2.js
@@ -5,10 +5,10 @@ import { ListItem } from '@occmundial/occ-atomic';
 export default function Example2() {
     return (
       <ul>
-            <ListItem icon="check" text="Miembro 1" />
-            <ListItem icon="cross" size="small" hierarchy={1} text="sub miembro jerarquía 1"/>
-            <ListItem icon="cross" size="small" hierarchy={2} text="sub miembro jerarquía 2" />
-            <ListItem icon="cross" size="small" hierarchy={3} text="sub miembro jerarquía 3"/>
+            <ListItem icon="cross" text="Miembro 1" />
+            <ListItem size="small" hierarchy={1} text="sub miembro jerarquía 1"/>
+            <ListItem size="small" hierarchy={2} text="sub miembro jerarquía 2" />
+            <ListItem size="small" hierarchy={3} text="sub miembro jerarquía 3"/>
       </ul>
     );
 }
--- src/docs/examples/ListItem/Example3.js
+++ src/docs/examples/ListItem/Example3.js
@@ -6,21 +6,21 @@ export default function Example3() {
     return (
         <ul>
           <Text>Bold/Italic Lines:</Text>
-          <ListItem icon="cross" bold text="*bold text*" />
-          <ListItem icon="cross" italic text="*italic text*" />
-          <ListItem icon="cross" crossed text="*crossed text*" />
-          <ListItem icon="cross" underlined text="*underlined text*" />
-          <ListItem icon="cross" bold text="This listItem has some *bold text* and some regular text." />
-          <ListItem icon="cross" italic text="This listItem has some *italic text* and some regular text." />
-          <ListItem icon="cross" italic bold text="*italic bold text*" />
-          <ListItem icon="cross" italic bold text="This listItem has some *italic bold text* and some regular text." />
+          <ListItem bold text="*bold text*" />
+          <ListItem italic text="*italic text*" />
+          <ListItem crossed text="*crossed text*" />
+          <ListItem underlined text="*underlined text*" />
+          <ListItem bold text="This listItem has some *bold text* and some regular text." />
+          <ListItem italic text="This listItem has some *italic text* and some regular text." />
+          <ListItem italic bold crossed text="*italic bold crossed text*" />
+          <ListItem italic bold underlined text="This listItem has some *italic bold underlined text* and some regular text." />
           <Text>Mid, low, white Lines:</Text>
-          <ListItem icon="cross" italic bold textColor="mid" text="Mid listItem." />
-          <ListItem icon="cross" italic bold textColor="low" text="Low listItem." />
+          <ListItem italic bold textColor="mid" text="Mid listItem." />
+          <ListItem italic bold textColor="low" text="Low listItem." />
           <ListItem style={{background:colors.prim}} icon="cross" iconColor={colors.bgWhite} italic bold textColor="white" text="White listItem." />
           <Text>Link Lines:</Text>
-          <ListItem icon="cross" href="https://www.google.com/" target="_blank"  linkStyle={{textDecoration: 'none', color: colors.textLink}} text="ListItem with a #link# to google." />
-          <ListItem icon="cross" italic bold href="https://www.google.com/" text="ListItem with a #link# to see the List Component and some *italic bold* text" />
+          <ListItem href="https://www.google.com/" target="_blank"  linkStyle={{textDecoration: 'none', color: colors.textLink}} text="ListItem with a #link# to google." />
+          <ListItem italic bold href="https://www.google.com/" text="ListItem with a #link# to see the List Component and some *italic bold* text" />
         </ul>
     );
 }
