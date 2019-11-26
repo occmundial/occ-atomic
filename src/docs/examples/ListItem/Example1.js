31e98fe442dc3bde0dacdca3a67e74ccef293ec2
Moises Rojas <irojas@occ.com.mx>
2019-11-26 15:25:26 -0600

    ListItem

--- src/docs/examples/ListItem/Example1.js
+++ src/docs/examples/ListItem/Example1.js
@@ -5,34 +5,16 @@ import { ListItem, Text, colors } from '@occmundial/occ-atomic';
 export default function Example1() {
     return (
         <ul>
-          <ListItem icon="check" iconColor={colors.prim} size="xLarge">
-            <Text hero>Miembro 1</Text>
-          </ListItem>
-          <ListItem icon="check" iconColor={colors.prim} size="large">
-            <Text headline>Miembro 2</Text>
-          </ListItem>
-          <ListItem icon="check" iconColor={colors.prim} size="medium">
-            <Text heading>Miembro 3</Text>
-          </ListItem>
-          <ListItem icon="cross" iconColor={colors.error} size="base">
-            <Text subheading>Miembro 4</Text>
-          </ListItem>
-          <ListItem icon="check">
-            <Text>Miembro 5</Text>
-          </ListItem>
-          <ListItem icon="cross" iconColor={colors.secDark} size="small">
-            <Text small>Miembro 6</Text>
-          </ListItem>
-          <ListItem icon="cross" size="small">
-            <Text micro>Miembro 7</Text>
-          </ListItem>
+          <ListItem icon="check" iconColor={colors.prim} size="headline" text="Miembro 1" />
+          <ListItem icon="check" iconColor={colors.prim} size="heading" text="Miembro 2" />
+          <ListItem icon="check" iconColor={colors.prim} size="subheading" text="Miembro 3" />
+          <ListItem icon="cross" iconColor={colors.error} size="large" text="Miembro 4" />
+          <ListItem icon="check" text="Miembro 5" />
+          <ListItem icon="cross" iconColor={colors.secDark} size="small" text="Miembro 6" />
+          <ListItem icon="cross" size="micro" text="Miembro 7" />
           <Text>Multiple Lines:</Text>
-          <ListItem icon="cross">
-            <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mauris justo, feugiat a erat vel, finibus accumsan lorem. Vestibulum ut commodo elit, eget viverra purus. Fusce feugiat libero ac felis vulputate egestas. Mauris non malesuada lacus. Fusce vitae tellus vel magna iaculis vehicula ac vitae mi. Vivamus tincidunt accumsan diam at semper. Fusce in orci eu sem fermentum fermentum sed at odio.</Text>
-          </ListItem>
-          <ListItem size="small" icon="cross">
-            <Text small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mauris justo, feugiat a erat vel, finibus accumsan lorem. Vestibulum ut commodo elit, eget viverra purus. Fusce feugiat libero ac felis vulputate egestas. Mauris non malesuada lacus. Fusce vitae tellus vel magna iaculis vehicula ac vitae mi. Vivamus tincidunt accumsan diam at semper. Fusce in orci eu sem fermentum fermentum sed at odio.</Text>
-          </ListItem>
+          <ListItem icon="cross" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mauris justo, feugiat a erat vel, finibus accumsan lorem. Vestibulum ut commodo elit, eget viverra purus. Fusce feugiat libero ac felis vulputate egestas. Mauris non malesuada lacus. Fusce vitae tellus vel magna iaculis vehicula ac vitae mi. Vivamus tincidunt accumsan diam at semper. Fusce in orci eu sem fermentum fermentum sed at odio." />
+          <ListItem size="small" icon="cross" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mauris justo, feugiat a erat vel, finibus accumsan lorem. Vestibulum ut commodo elit, eget viverra purus. Fusce feugiat libero ac felis vulputate egestas. Mauris non malesuada lacus. Fusce vitae tellus vel magna iaculis vehicula ac vitae mi. Vivamus tincidunt accumsan diam at semper. Fusce in orci eu sem fermentum fermentum sed at odio." />
         </ul>
     );
 }
--- src/docs/examples/ListItem/Example3.js
+++ src/docs/examples/ListItem/Example3.js
@@ -1,22 +1,26 @@
-/* Render a list with a list as children */
+/* Using the diferent styles in the listItem */
 import React from 'react';
-import { ListItem, Text } from '@occmundial/occ-atomic';
+import { ListItem, Text, colors } from '@occmundial/occ-atomic';
 
-export default function Example2() {
+export default function Example3() {
     return (
         <ul>
-           <ListItem icon="check">
-            <Text>Miembro 1</Text>
-          </ListItem>
-          <ListItem icon="cross" size="small" hierarchy={1}>
-                <Text micro>sub miembro jerarquía 1</Text>
-          </ListItem>
-          <ListItem icon="cross" size="small" hierarchy={2}>
-                <Text micro>sub miembro jerarquía 2</Text>
-          </ListItem>
-          <ListItem icon="cross" size="small" hierarchy={3}>
-                <Text micro>sub miembro jerarquía 3</Text>
-          </ListItem>
+          <Text>Bold/Italic Lines:</Text>
+          <ListItem icon="cross" bold text="*bold text*" />
+          <ListItem icon="cross" italic text="*italic text*" />
+          <ListItem icon="cross" crossed text="*crossed text*" />
+          <ListItem icon="cross" underlined text="*underlined text*" />
+          <ListItem icon="cross" bold text="This listItem has some *bold text* and some regular text." />
+          <ListItem icon="cross" italic text="This listItem has some *italic text* and some regular text." />
+          <ListItem icon="cross" italic bold text="*italic bold text*" />
+          <ListItem icon="cross" italic bold text="This listItem has some *italic bold text* and some regular text." />
+          <Text>Mid, low, white Lines:</Text>
+          <ListItem icon="cross" italic bold textColor="mid" text="Mid listItem." />
+          <ListItem icon="cross" italic bold textColor="low" text="Low listItem." />
+          <ListItem style={{background:colors.prim}} icon="cross" iconColor={colors.bgWhite} italic bold textColor="white" text="White listItem." />
+          <Text>Link Lines:</Text>
+          <ListItem icon="cross" href="https://www.google.com/" target="_blank"  linkStyle={{textDecoration: 'none', color: colors.textLink}} text="ListItem with a #link# to google." />
+          <ListItem icon="cross" italic bold href="https://www.google.com/" text="ListItem with a #link# to see the List Component and some *italic bold* text" />
         </ul>
     );
 }
