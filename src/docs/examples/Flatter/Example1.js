569073607a965c139473aa86eae214b953e1173a
Jesús W. Gaona <jgaona@occ.com.mx>
2019-11-27 15:40:31 -0600

    Se implementa version final de Flatter

--- src/docs/examples/Flatter/Example1.js
+++ src/docs/examples/Flatter/Example1.js
@@ -11,6 +11,15 @@ const htmlTextExample =
 <ul>
 <li>LI tag # 1</li>
 <li>LI tag # 2</li>
+<li>LI tag # 3 <strong><em>EM tag inside a strong tag text</em></strong></li>
+<li>LI tag # 4 <em>EM tag text</em> <strong>STRONG tag text</strong></li>
+<li>LI tag # 5 <p>Text inside p</p></li>
+<li>LI tag # 6 <p>Text before BR<br> Text after BR</p> inside LI</li>
+<li>
+<ul>
+<li>LI tag # 8 hierarchy 2 inside another li tag</li>
+</ul>
+</li>
 </ul>
 <p>Text before BR<br>Text after BR</p>`;
 
