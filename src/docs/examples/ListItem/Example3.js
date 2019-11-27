5885e1cba837b2d48b98da24b97178109d899590
Moises Rojas <irojas@occ.com.mx>
2019-11-27 10:13:03 -0600

    listItem

--- src/docs/examples/ListItem/Example3.js
+++ src/docs/examples/ListItem/Example3.js
@@ -6,21 +6,13 @@ export default function Example3() {
     return (
         <ul>
           <Text>Bold/Italic Lines:</Text>
-          <ListItem bold text="*bold text*" />
-          <ListItem italic text="*italic text*" />
-          <ListItem crossed text="*crossed text*" />
-          <ListItem underlined text="*underlined text*" />
-          <ListItem bold text="This listItem has some *bold text* and some regular text." />
-          <ListItem italic text="This listItem has some *italic text* and some regular text." />
-          <ListItem italic bold crossed text="*italic bold crossed text*" />
-          <ListItem italic bold underlined text="This listItem has some *italic bold underlined text* and some regular text." />
-          <Text>Mid, low, white Lines:</Text>
-          <ListItem italic bold textColor="mid" text="Mid listItem." />
-          <ListItem italic bold textColor="low" text="Low listItem." />
-          <ListItem style={{background:colors.prim}} icon="cross" iconColor={colors.bgWhite} italic bold textColor="white" text="White listItem." />
-          <Text>Link Lines:</Text>
-          <ListItem href="https://www.google.com/" target="_blank"  linkStyle={{textDecoration: 'none', color: colors.textLink}} text="ListItem with a #link# to google." />
-          <ListItem italic bold href="https://www.google.com/" text="ListItem with a #link# to see the List Component and some *italic bold* text" />
+          <ListItem child={[{text: "bold text", props:{bold: true}}]} />
+          <ListItem child={[{text: "italic text", props:{italic: true}}]} />
+          <ListItem child={[{text: "crossed text", props:{crossed: true}}]} crossed text="*crossed text*" />
+          <ListItem child={[{text: "underlined text", props:{underlined: true}}]} />
+          <ListItem child={[{text: "bold text ", props:{bold: true}}, {text: "italic text", props:{italic: true}}]} />
+          <ListItem child={[{text: "bold underlined text ", props:{bold: true, underlined: true}}, {text: "italic crossed text", props:{italic: true, crossed: true }}]} />
+          <ListItem child={[{text: "This text has a " }, {text: "link", link:{href: "https://google.com", target: '_blank', linkStyle: {color: colors.textLink, textDecoration: 'none'}}},{text: " <- right there" },]} />
         </ul>
     );
 }
