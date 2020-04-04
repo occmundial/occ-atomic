c6cbc70374cc8547ddedb18427b256d1c28e350a
Luis Alberto <luis.hernandez@occ.com.mx>
2020-04-04 14:32:53 -0600

    Se sube ejemplo

---
+++ src/docs/examples/Banner/Example1.js
@@ -0,0 +1,18 @@
+/* Themes of Tip */
+import React, { Fragment } from 'react';
+import { Banner, colors } from '@occmundial/occ-atomic';
+
+export default function Example1() {
+    return (
+        <div>
+            <Banner >
+                <Fragment>      
+                    ¿Necesitas ayuda? Contamos con Atención al Cliente las 24 horas del día.
+                    <a href="javascript:void(0)" style={{ color: colors.bgWhite, textDecoration: 'underline', marginLeft: '8px', cursor: 'pointer' }}>
+                        Saber más
+                    </a>
+                </Fragment>
+            </Banner>
+        </div>
+    );
+}
