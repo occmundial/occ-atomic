9fbef83a56d9bcae9bde23f67c8047fbd66eda14
Miguel Malagon <miguelmalagon@outlook.com>
2020-05-12 17:30:45 -0500

    add styles in text tag

--- src/docs/examples/Tag/Example4.js
+++ src/docs/examples/Tag/Example4.js
@@ -3,14 +3,26 @@ import React from 'react';
 import { Tag } from '@occmundial/occ-atomic';
 
 export default function Example1() {
-  return (
-    <div>
-      <Tag iconName="check" size="medium">Default Tag</Tag>
-      <Tag theme="info" iconName="check" size="medium">Tag with info theme</Tag>
-      <Tag theme="success" iconName="check" size="medium">Tag with success theme</Tag>
-      <Tag theme="warning" iconName="check" size="medium">Tag with warning theme</Tag>
-      <Tag theme="error" iconName="check" size="medium">Tag with error theme</Tag>
-      <Tag theme="link" iconName="check" size="medium">Tag with link theme</Tag>
-    </div>
-  );
+	return (
+		<div>
+			<Tag iconName="check" size="medium">
+				Default Tag
+			</Tag>
+			<Tag theme="info" iconName="check" size="medium">
+				Tag with info theme
+			</Tag>
+			<Tag theme="success" iconName="check" size="medium">
+				Tag with success theme
+			</Tag>
+			<Tag theme="warning" iconName="check" size="medium">
+				Tag with warning theme
+			</Tag>
+			<Tag theme="error" iconName="check" size="medium">
+				Tag with error theme
+			</Tag>
+			<Tag theme="link" iconName="check" size="medium">
+				Tag with link theme
+			</Tag>
+		</div>
+	);
 }
