51431ad61baea2753d9c2f89bcad98136d6bb752
Miguel Malagon <miguelmalagon@outlook.com>
2020-05-13 11:26:20 -0500

    add big size examples

---
+++ src/docs/examples/Tag/Example5.js
@@ -0,0 +1,26 @@
+/* Themes without Icon big size */
+import React from 'react';
+import { Tag } from '@occmundial/occ-atomic';
+
+export default function Example1() {
+  return (
+    <div>
+      <Tag size="big">Default Tag</Tag>
+      <Tag theme="info" size="medium">
+        Tag with info theme
+      </Tag>
+      <Tag theme="success" size="big">
+        Tag with success theme
+      </Tag>
+      <Tag theme="warning" size="big">
+        Tag with warning theme
+      </Tag>
+      <Tag theme="error" size="big">
+        Tag with error theme
+      </Tag>
+      <Tag theme="link" size="big">
+        Tag with link theme
+      </Tag>
+    </div>
+  );
+}
