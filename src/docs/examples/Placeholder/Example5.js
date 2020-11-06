da10126c586b89df3fd2341d5fd951375b05ab47
LupitaL <mlopez@occ.com.mx>
2020-11-06 14:02:25 -0600

    Se aguardan cambios para componente

---
+++ src/docs/examples/Placeholder/Example5.js
@@ -0,0 +1,11 @@
+/* Example of layout loading */
+import React from 'react';
+import { Placeholder } from '@occmundial/occ-atomic';
+
+export default function Example5() {
+	return (
+		<div>
+			<Placeholder start />
+		</div>
+	);
+}
