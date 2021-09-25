9a40cd149d5d235328d008969fca8c3f16d74a14
Luis Fernando Díaz <ldiaz@occ.com.mx>
2021-09-24 23:57:53 -0500

    build: Replace highlight.js to prism-react-renderer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './docs/App';
-import '../node_modules/highlight.js/styles/atom-one-dark.css';
 
 ReactDOM.render(<App />, document.getElementById('app'));
