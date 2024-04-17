3efe252c3d8651976f28e951081c717861c0ad56
Emmanuel Yañez <118850958+emmanuel-yanez-occ@users.noreply.github.com>
2024-04-17 15:38:02 -0600

    feat: Changed some examples with tables

--- src/docs/examples/Text/Example4.js
+++ src/docs/examples/Text/Example4.js
@@ -1,35 +1,88 @@
 /* Spacing */
 import React from 'react';
 import { Text } from '@occmundial/occ-atomic';
+import ExampleTable from '../../ExampleTable';
 
 export default function Example4() {
     return (
-        <div style={{ display: 'flex' }}>
-            <div style={{ flex: 1 }}>
+        <ExampleTable>
+            <thead>
+                <tr>
+                    <th>Prop</th>
+                    <th>Prop (Legacy)</th>
+                </tr>
+            </thead>
+            <tbody>
                 {/* You can choose between top and bottom */}
-                <Text bottom0>Bottom0</Text>
-                <Text bottom1>Bottom1</Text>
-                <Text bottom2>Bottom2</Text>
-                <Text bottom3>Bottom3</Text>
-                <Text bottom4>Bottom4</Text>
-                <Text bottom5>Bottom5</Text>
-                <Text bottom6>Bottom6</Text>
-                <Text bottom7>Bottom7</Text>
-                <Text bottom8>Bottom8</Text>
-                <Text bottom9>Bottom9</Text>
-                <Text bottom10>Bottom10</Text>
-                <Text bottom11>Bottom11</Text>
-                <Text bottom12>Bottom12</Text>
-            </div>
-            <div style={{ flex: 1 }}>
-                <Text bottomXTiny>X-Tiny</Text>
-                <Text bottomTiny>Tiny</Text>
-                <Text bottomSmall>Small</Text>
-                <Text bottomBase>Base</Text>
-                <Text bottomMedium>Medium</Text>
-                <Text bottomLarge>Large</Text>
-                <Text bottomXLarge>XLarge</Text>
-            </div>
-        </div>
+
+                <tr>
+                    <td><Text bottom0>Bottom0</Text></td>
+                    <td/>
+                </tr>
+
+                <tr>
+                    <td><Text bottom1>Bottom1</Text></td>
+                    <td><Text bottomXTiny>X-Tiny</Text></td>
+                </tr>
+
+                <tr>
+                    <td><Text bottom2>Bottom2</Text></td>
+                    <td><Text bottomTiny>Tiny</Text></td>
+                </tr>
+
+                <tr>
+                    <td><Text bottom3>Bottom3</Text></td>
+                    <td/>
+                </tr>
+
+
+                <tr>
+                    <td><Text bottom4>Bottom4</Text></td>
+                    <td><Text bottomSmall>Small</Text></td>
+                </tr>
+
+                <tr>
+                    <td><Text bottom5>Bottom5</Text></td>
+                    <td><Text bottomBase>Base</Text></td>
+                </tr>
+
+                <tr>
+                    <td><Text bottom6>Bottom6</Text></td>
+                    <td><Text bottomMedium>Medium</Text></td>
+                </tr>
+
+                <tr>
+                    <td><Text bottom7>Bottom7</Text></td>
+                    <td/>
+                </tr>
+
+                <tr>
+                    <td><Text bottom8>Bottom8</Text></td>
+                    <td><Text bottomLarge>Large</Text></td>
+                </tr>
+
+                <tr>
+                    <td><Text bottom9>Bottom9</Text></td>
+                    <td><Text bottomXLarge>XLarge</Text></td>
+                </tr>
+
+                <tr>
+                    <td><Text bottom10>Bottom10</Text></td>
+                    <td/>
+                </tr>
+
+                <tr>
+                    <td><Text bottom11>Bottom11</Text></td>
+                    <td/>
+                </tr>
+
+                <tr>
+                    <td><Text bottom12>Bottom12</Text></td>
+                    <td/>
+                </tr>
+
+            </tbody>
+
+        </ExampleTable>
     );
 }
