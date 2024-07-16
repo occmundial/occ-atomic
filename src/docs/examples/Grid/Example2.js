97e839aa4621fcbb964615167165132628b646c3
JaviOCC <jlopezg@occ.com.mx>
2024-07-16 11:06:26 -0700

    fix: Remove unnecesary bp props in grid col

--- src/docs/examples/Grid/Example2.js
+++ src/docs/examples/Grid/Example2.js
@@ -8,11 +8,6 @@ function Example2({ winWidth }) {
       <Grid.Row>
         <Grid.Col
           xxs={{ col: 3 }}
-          xs={{ col: 6 }}
-          sm={{ col: 3 }}
-          md={{ col: 3 }}
-          lg={{ col: 3 }}
-          xl={{ col: 3 }}
         >
           <Card
             noPadding={winWidth < grid.xs}
@@ -24,11 +19,6 @@ function Example2({ winWidth }) {
         </Grid.Col>
         <Grid.Col
           xxs={{ col: 3 }}
-          xs={{ col: 6 }}
-          sm={{ col: 3 }}
-          md={{ col: 3 }}
-          lg={{ col: 3 }}
-          xl={{ col: 3 }}
         >
           <Card
             noPadding={winWidth < grid.xs}
@@ -40,11 +30,6 @@ function Example2({ winWidth }) {
         </Grid.Col>
         <Grid.Col
           xxs={{ col: 3 }}
-          xs={{ col: 6 }}
-          sm={{ col: 3 }}
-          md={{ col: 3 }}
-          lg={{ col: 3 }}
-          xl={{ col: 3 }}
         >
           <Card
             noPadding={winWidth < grid.xs}
@@ -56,11 +41,6 @@ function Example2({ winWidth }) {
         </Grid.Col>
         <Grid.Col
           xxs={{ col: 3 }}
-          xs={{ col: 6 }}
-          sm={{ col: 3 }}
-          md={{ col: 3 }}
-          lg={{ col: 3 }}
-          xl={{ col: 3 }}
         >
           <Card
             noPadding={winWidth < grid.xs}
