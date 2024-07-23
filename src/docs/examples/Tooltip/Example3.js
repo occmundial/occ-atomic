129de69961d2dc398073843595e0d454d52e2804
JaviOCC <jlopezg@occ.com.mx>
2024-07-23 16:43:47 -0700

    test: Update placement bottom example

--- src/docs/examples/Tooltip/Example3.js
+++ src/docs/examples/Tooltip/Example3.js
@@ -11,12 +11,12 @@ export default function Example3() {
 			wrap="wrap"
 		>
 			<Flexbox flex-direction="col">
-				<div style={{ display: 'flex', flexDirection: 'column', gap: '70px' }}>
+				<div style={{ display: 'flex', flexDirection: 'column', gap: '70px', alignItems: 'center' }}>
 					<Tooltip placement="bottom" text="Bottom" open theme="dark">
 						<Button size="md">Button with large content</Button>
 					</Tooltip>
 					<Tooltip placement="bottom-start" text="Bottom-start large content" open theme="dark">
-						<Button size="md">Button with large content</Button>
+						<Button size="md">Button with </Button>
 					</Tooltip>
 					<div style={{ marginTop: '8px' }}>
 						<Tooltip placement="bottom-end" text="Botton-end" open theme="dark">
