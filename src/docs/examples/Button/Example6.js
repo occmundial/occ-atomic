d8bc73881275ec1537e5a06bd37f9a869a5fe210
emmanuel-yanez-occ <ryanez@occ.com.mx>
2024-07-25 14:09:59 -0600

    fix: Icon disabled styles

--- src/docs/examples/Button/Example6.js
+++ src/docs/examples/Button/Example6.js
@@ -1,9 +1,21 @@
 /* Button disabled */
 import React from 'react';
-import { Button } from '@occmundial/occ-atomic';
+import { Button, colors } from '@occmundial/occ-atomic';
 
 export default function Example6() {
     return (
-        <Button disabled>Disabled</Button>
+        <div style={{ display: 'flex', gap: '8px', flexWrap: 'wrap', alignItems: 'baseline' }}>
+            <Button icon="search" iconRight="search" theme="primary" disabled>Disabled</Button>
+            <Button icon="search" iconRight="search" theme="secondary" disabled>Disabled</Button>
+            <Button icon="search" iconRight="search" theme="tertiary" disabled>Disabled</Button>
+            <div style={{background:colors.prim, marginTop:'15px', padding:'10px', display:'inline-block'}}>
+                <Button icon="search" iconRight="search" theme="tertiaryWhite" disabled>Disabled</Button>
+            </div>
+            <Button icon="search" iconRight="search" theme="ghostPink" disabled>Disabled</Button>
+            <Button icon="search" iconRight="search" theme="ghostGrey" disabled>Disabled</Button>
+            <div style={{background:colors.prim, marginTop:'15px', padding:'10px', display:'inline-block'}}>
+                <Button icon="search" iconRight="search" theme="ghostWhite" disabled>Disabled</Button>
+            </div>
+        </div>
     );
 }
