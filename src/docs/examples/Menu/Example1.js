18e5ae80ef60d6da6d7cb3a7fd04492cc3ae9b4f
Luis Fernando Díaz <ldiaz@occ.com.mx>
2024-10-09 17:13:11 -0600

    feat: Implement triggerRef into the Menu component

--- src/docs/examples/Menu/Example1.js
+++ src/docs/examples/Menu/Example1.js
@@ -1,28 +1,57 @@
 /* Menu */
-import React from 'react';
-import { Menu, Button } from '@occmundial/occ-atomic';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Menu, Button, NavAvatarButton, WindowSize, grid, Drawer, NavTab } from '@occmundial/occ-atomic';
 
-export default function Example1() {
+function Example1({ winWidth }) {
+  const [open, setOpen] = useState(false);
+  const [reference, setReference] = useState(null);
+  const isMobile = winWidth < grid.xs;
+  console.log({ winWidth, isMobile, grid, reference });
+
   return (
     <div style={{ position: 'relative' }}>
-      <Menu
-        placement="bottom-start"
-        triggerElement={
-          (
-            <div>
-              <Button
-                theme="ghostGrey"
-              >Open</Button>
-            </div>
-          )
+      <NavAvatarButton
+        theme="ghost"
+        photo="https://i.pravatar.cc/300"
+        onClick={() => setOpen(open => !open)}
+        ref={setReference}
+      />
+      {!isMobile ? (
+        <Menu
+          placement="bottom-start"
+          open={open}
+          setOpen={setOpen}
+          triggerRef={reference}
+        >
+          <div style={{ display: 'flex', flexDirection: 'column', rowGap: 12 }}>
+            <Button>Prueba menu item 1</Button>
+            <Button>Prueba menu item 2</Button>
+            <Button>Prueba menu item 3</Button>
+            <Button>Prueba menu item 4</Button>
+          </div>
+        </Menu>
+      ) : (
+        <Drawer header={
+          <NavTab left={[
+            {
+              key: 'icon',
+              type: 'navButton',
+              iconName: 'x',
+              onClick: () => setOpen(false)
+            }
+          ]}
+          />
         }>
-        <div style={{ display: 'flex', flexDirection: 'column', rowGap: 12 }}>
-          <Button>Prueba menu item 1</Button>
-          <Button>Prueba menu item 2</Button>
-          <Button>Prueba menu item 3</Button>
-          <Button>Prueba menu item 4</Button>
-        </div>
-      </Menu>
+          This is a default Drawer
+        </Drawer>
+      )}
     </div>
   );
 }
+
+Example1.propTypes = {
+  winWidth: PropTypes.number
+};
+
+export default WindowSize(Example1);
